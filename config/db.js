39cf023a0ab7b465fb78ea2d7d9a09c0edb4a5be
Prem Kumar <prem68265@gmail.com>
2024-09-15 17:25:53 +0530

    database changed

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   
-  const DB= "mongodb+srv://prem:Prem650019@cluster0.6snqz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  const DB= "mongodb+srv://prem:Prem650019@cluster0.1vv64.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
   mongoose.connect(DB).then(()=>{
       console.log("database connected")
   }).catch((err)=>console.log(err))
